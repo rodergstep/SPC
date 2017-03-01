a6fba5ff1fc5bc9bcd679c71e743704fb1f82fa5
Bienov Roman <rodergstep@gmail.com>
2017-03-01 15:53:29 +0200

    Create js_snippets.js

---
+++ js_snippets.js
@@ -0,0 +1,7 @@
+// Випадкове ціле число від min до max включно:
+// var rand = min + Math.floor(Math.random() * (max + 1 - min));
+// _____________________________________________________________
+// Перевірка на число:
+// function isNumeric(n) {
+//  return !isNaN(parseFloat(n)) && isFinite(n)
+// }
