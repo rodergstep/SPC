c8bbbf2b5b9d156f8f0f00cb8f579ed5dc3dc149
Bienov Roman <rodergstep@gmail.com>
2017-03-08 13:52:36 +0200

    Update js_snippets.js

--- js_snippets.js
+++ js_snippets.js
@@ -69,3 +69,41 @@ _________________________________________________________________
 function getClass(obj) {
   return {}.toString.call(obj).slice(8, -1);
 }
+_________________________________________________________________
+Просунута техніка для відловлювання помилок:
+function ReadError(message, cause) {
+  this.message = message;
+  this.cause = cause;
+  this.name = 'ReadError';
+  this.stack = cause.stack;
+}
+
+function readData() {
+  var data = '{ bad data }';
+
+  try {
+    // ...
+    JSON.parse(data);
+    // ...
+  } catch (e) {
+    // ...
+    if (e.name == 'URIError') {
+      throw new ReadError("Ошибка в URI", e);
+    } else if (e.name == 'SyntaxError') {
+      throw new ReadError("Синтаксическая ошибка в данных", e);
+    } else {
+      throw e; // пробрасываем
+    }
+  }
+}
+
+try {
+  readData();
+} catch (e) {
+  if (e.name == 'ReadError') {
+    alert( e.message );
+    alert( e.cause ); // оригинальная ошибка-причина
+  } else {
+    throw e;
+  }
+}
